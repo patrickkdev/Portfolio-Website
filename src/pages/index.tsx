b9b493e293d1c006f8b0659de3d58e23f5807432
patrickkdev <patrick8q@gmail.com>
2025-10-09 12:14:17 -0300

    Removed BlogSection import and getServerSideProps function from index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import AboutSection from '@/components/partials/AboutSection';
-import BlogSection from '@/components/partials/BlogSection';
 import ContactSection from '@/components/partials/ContactSection';
 import ExperienceSection from '@/components/partials/ExperienceSection';
 import HeroSection from '@/components/partials/HeroSection';
@@ -7,12 +6,7 @@ import RecentWorkSection from '@/components/partials/RecentWorkSection';
 import ServiceSection from '@/components/partials/ServiceSection';
 import TestimonialSection from '@/components/partials/TestimonialSection';
 import AppLayout from '@/layouts/AppLayout';
-import { api } from '@/lib/api';
-import type { GetServerSideProps, NextPage } from 'next';
-
-// type Props = {
-//   experiences: ;
-// }
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
@@ -42,19 +36,5 @@ const Home: NextPage = () => {
     </AppLayout>
   );
 };
-
-// export const getServerSideProps: GetServerSideProps = async ({}) => {
-//   const getExperiences = api.get('/experiences').then((res) => res.data.data);
-//   const getEducations = api.get('/educations').then((res) => res.data.data);
-//   const profile = api.get('/profile').then((res) => res.data.data);
-//   const result = await Promise.all([getExperiences, getEducations, profile]);
-//   return {
-//     props: {
-//       experiences: result[0],
-//       educations: result[1],
-//       profile: result[2],
-//     },
-//   };
-// };
 
 export default Home;
