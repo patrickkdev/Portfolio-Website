fdafabba64ad91ed5ed1a0f48099204c5633565d
patrickkdev <patrick8q@gmail.com>
2025-10-22 15:00:31 -0300

    fix: added semicolons at end of import statements in HeroSection.tsx

--- src/components/partials/HeroSection.tsx
+++ src/components/partials/HeroSection.tsx
@@ -1,10 +1,10 @@
-import Button from '@/components/form/Button'
-import Image from 'next/image'
-import Link from 'next/link'
-import { BsDiscord, BsWhatsapp } from 'react-icons/bs'
-import { FiGithub, FiLinkedin } from 'react-icons/fi'
-import { ReactTyped } from 'react-typed'
-import HeroBackground from './HeroBackground'
+import Button from '@/components/form/Button';
+import Image from 'next/image';
+import Link from 'next/link';
+import { BsDiscord, BsWhatsapp } from 'react-icons/bs';
+import { FiGithub, FiLinkedin } from 'react-icons/fi';
+import { ReactTyped } from 'react-typed';
+import HeroBackground from './HeroBackground';
 
 const HeroSection = () => {
   return (
