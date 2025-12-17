6a42deeb92b2d03f3ad5d2b151bd81ab31a6ebb8
patrickkdev <patrick8q@gmail.com>
2025-12-17 17:43:43 -0300

    refactor: remove unused environment variables from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
-    TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
-  }
 };
 
 export default nextConfig;
